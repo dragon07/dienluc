b6439c4d3314f4a170b7fba15361dec1304b42ff
Khac Chien <chiennk07@gmail.com>
2021-10-29 10:32:07 +0700

    update-4

--- resources/views/admin/nhan-vien/index.blade.php
+++ resources/views/admin/nhan-vien/index.blade.php
@@ -4,9 +4,9 @@
     <div class="d-md-flex justify-content-between mb-3">
         <form class="form-inline mb-2 mb-md-0">
             <input type="text" class="form-control mr-sm-2" name="key" value="" placeholder="Nhập tên">
-            <button type="submit" class="btn btn-primary">Search</button>
+            <button type="submit" class="btn btn-primary">Tìm kiếm</button>
         </form>
-        <a class="btn btn-success" href="{{route('nha-vien.create')}}">
+        <a class="btn btn-success" href="{{route('nhan-vien.create')}}">
             <i class="fal fa-plus-circle"></i>
             Thêm mới
         </a>
@@ -15,11 +15,11 @@
         <table class="table">
             <thead class="thead-light">
             <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Name</th>
-                <th scope="col">Total product</th>
-                <th scope="col">Status</th>
-                <th scope="col">Created date</th>
+                <th scope="col">STT</th>
+                <th scope="col">Họ tên</th>
+                <th scope="col">MST</th>
+                <th scope="col">Số hiệu HRMS</th>
+                <th scope="col">Ngày tạo</th>
                 <th scope="col" class="text-right">Action</th>
             </tr>
             </thead>
